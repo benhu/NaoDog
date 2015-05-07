8ac14e3454a5089424c335055c5b4754afac1a15
Benjamin <benjamin.humeau@epsi.fr>
2015-05-07 08:49:15 +0200

    Init

---
+++ build-naodogtoolchain/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/benjamin/Documents/Workspace/NaoDog
+# Build directory: /home/benjamin/Documents/Workspace/NaoDog/build-naodogtoolchain
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
